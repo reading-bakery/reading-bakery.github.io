7d2ed2b558f622f94f2d26247931a327d335c1a2
K.K. <katjakotschurin@gmail.com>
2025-06-13 20:27:26 +0200

    edits

--- cards.js
+++ cards.js
@@ -41,139 +41,139 @@ const recipes = [
     img: "/images/gefülltepaprika.png",
     url: "gefuelltepaprikahack.html"
   },
-    {
+  {
     title: "Tortellini Auflauf",
     description: "Mittel • 50 Min. • 4 Personen",
     img: "/images/tortelliniauflauf.png",
     url: "tortelliniauflauf.html"
   },
-    {
+  {
     title: "Kartoffelgulasch mit Würstchen",
     description: "Mittel • 25 Min. • 2 Personen",
     img: "/images/kartoffelgulasch.png",
     url: "kartoffelgulasch_mit_wuerstchen.html"
   },
-    {
+  {
     title: "Würstchengulasch",
     description: "Leicht • 20 Min. • 3 Personen",
     img: "/images/wuerstchengulasch.png",
     url: "wuerstchengulasch.html"
   },
-    {
+  {
     title: "Paprika-Reispfanne mit Joghurtsauce",
     description: "Mittel • 45 Min. • 4 Personen",
     img: "/images/reispfanne.png",
     url: "reispfanne.html"
   },
-    {
+  {
     title: "Currysuppe",
     description: "Leicht • 15 Min. • 4 Personen",
     img: "/images/currysuppe.png",
     url: "currysuppe.html"
   },
-    {
+  {
     title: "Zitronenrisotto",
     description: "Mittel • 29 Min. • 4 Personen",
     img: "/images/zitronenrisotto.png",
     url: "zitronenrisotto.html"
   },
-    {
+  {
     title: "Gnocchi Auflauf",
     description: "Mittel • 35 Min. • 4 Personen",
     img: "/images/gnocchiauflauf.png",
     url: "gnocchiauflauf.html"
   },
-    {
+  {
     title: "Portobello Caprese",
     description: "Leicht • 20 Min. • 2 Personen",
     img: "/images/portobello.png",
     url: "portobello.html"
   },
-    {
+  {
     title: "Bandnudeln mit Spinat und Lachs",
     description: "Leicht • 20 Min. • 3 Personen",
     img: "/images/pastalachsspinat.png",
     url: "bandnudelnlachsspinat.html"
   },
-    {
+  {
     title: "Spaghetti mit Kirschtomaten und Garnelen",
     description: "Leicht • 30 Min. • 2 Personen",
     img: "/images/pastagarnelen.png",
     url: "spaghettigarnelen.html"
   },
-    {
+  {
     title: "Wirsingrouladen",
     description: "Miitel • 55 Min. • 3 Personen",
     img: "/images/wirsingroulade.png",
     url: "wirsingroulade.html"
   },
-    {
+  {
     title: "Spitzkohlcurry",
     description: "Leicht • 25 Min. • 4 Personen",
     img: "/images/spitzkohlcurry.png",
     url: "spitzkohlcurry.html"
   },
-    {
+  {
     title: "Cannelloni mit Spargel",
     description: "Leicht • 30 Min. • 2 Personen",
     img: "/images/cannellonispargel.png",
     url: "cannellonispargel.html"
   },
-    {
+  {
     title: "Pasta mit Wirsing",
     description: "Leicht • 30 Min. • 3 Personen",
     img: "/images/wirsingpasta.png",
     url: "wirsingpasta.html"
   },
-    {
+  {
     title: "Albondigas",
     description: "Leicht • 30 Min. • 4 Personen",
     img: "/images/albondigas.png",
     url: "albondigas.html"
   },
-    {
+  {
     title: "Pilzrisotto",
     description: "Mittel • 20 Min. • 2 Personen",
     img: "/images/pilzrisotto.png",
     url: "pilzrisotto.html"
   },
-    {
+  {
     title: "Tomate Mozzarella Gnocchi",
     description: "Leicht • 20 Min. • 3 Personen",
     img: "/images/gnocchimozzarella.png",
     url: "gnocchitomatemozarella.html"
   },
-    {
+  {
     title: "Fenchel nepalesisch",
     description: "Leicht • 20 Min. • 2 Personen",
     img: "/images/fenchelnepalesisch.png",
     url: "fenchelnepalesisch.html"
   },
-    {
+  {
     title: "Spargel in rosa Basilikum-Sauce",
     description: "Leicht • 30 Min. • 2 Personen",
     img: "/images/spargelkaesebasilikum.png",
     url: "spargelkaesesauce.html"
   },
-    {
+  {
     title: "Auberginen-Curry mit Joghurtsauce",
     description: "Leicht • 20 Min. • 2 Personen",
     img: "/images/auberginencurry.png",
     url: "auberginencurry.html"
   },
-    {
+  {
     title: "Couscouspfanne",
     description: "Leicht • 25 Min. • 2 Personen",
     img: "/images/couscouspfanne.png",
     url: "couscouspfanne.html"
   },
-    {
-    title: ""Borsch  / Борщ,
+  {
+    title: "Borsch  / Борщ,
     description: "Mittel • 35 Min. • 4 Personen",
     img: "/images/borsch.png",
     url: "borsch.html"
   },
-    {
+  {
     title: "Kohlrabi mit Sahnesauce",
     description: "Leicht • 40 Min. • 4 Personen",
     img: "/images/kohlrabigemuese.png",
