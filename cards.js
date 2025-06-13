05b8133cb7c91998c7faf4749a50197a30a3d9d6
K.K. <katjakotschurin@gmail.com>
2025-06-13 22:48:12 +0200

    finally

--- cards.js
+++ cards.js
@@ -13,277 +13,277 @@ const recipes = [
   },
   {
     title: "Brokkolisuppe",
-    description: "Leicht • 45 Min. • 4 Personen",
+    description: "Leicht • 45 Min.",
     img: "/images/brokkolisuppe.png",
     url: "brokkolisuppe.html"
   },
   {
     title: "Tomatensuppe",
-    description: "Leicht • 20 Min. • 2 Personen",
+    description: "Leicht • 20 Min.",
     img: "/images/tomatensuppe.png",
     url: "tomatensuppe.html"
   },
   {
     title: "Thai Curry Suppe",
-    description: "Leicht • 30 Min. • 4 Personen",
+    description: "Leicht • 30 Min.",
     img: "/images/thaicurrysuppe.png",
     url: "thaisuppe.html"
   },
   {
     title: "Fleischbällchen Toskana",
-    description: "Leicht • 55 Min. • 3 Personen",
+    description: "Leicht • 55 Min.",
     img: "/images/toskana.png",
     url: "toskana.html"
   },
   {
     title: "Gefüllte Paprika mit Hack",
-    description: "Mittel • 1 Std. 15 Min. • 4 Personen",
+    description: "Mittel • 1 Std. 15 Min.",
     img: "/images/gefülltepaprika.png",
     url: "gefuelltepaprikahack.html"
   },
   {
     title: "Tortellini Auflauf",
-    description: "Mittel • 50 Min. • 4 Personen",
+    description: "Mittel • 50 Min.",
     img: "/images/tortelliniauflauf.png",
     url: "tortelliniauflauf.html"
   },
   {
     title: "Kartoffelgulasch mit Würstchen",
-    description: "Mittel • 25 Min. • 2 Personen",
+    description: "Mittel • 25 Min.",
     img: "/images/kartoffelgulasch.png",
     url: "kartoffelgulasch_mit_wuerstchen.html"
   },
   {
     title: "Würstchengulasch",
-    description: "Leicht • 20 Min. • 3 Personen",
+    description: "Leicht • 20 Min.",
     img: "/images/wuerstchengulasch.png",
     url: "wuerstchengulasch.html"
   },
   {
     title: "Paprika-Reispfanne mit Joghurtsauce",
-    description: "Mittel • 45 Min. • 4 Personen",
+    description: "Mittel • 45 Min.",
     img: "/images/reispfanne.png",
     url: "reispfanne.html"
   },
   {
     title: "Currysuppe",
-    description: "Leicht • 15 Min. • 4 Personen",
+    description: "Leicht • 15 Min.",
     img: "/images/currysuppe.png",
     url: "currysuppe.html"
   },
   {
     title: "Zitronenrisotto",
-    description: "Mittel • 29 Min. • 4 Personen",
+    description: "Mittel • 29 Min.",
     img: "/images/zitronenrisotto.png",
     url: "zitronenrisotto.html"
   },
   {
     title: "Gnocchi Auflauf",
-    description: "Mittel • 35 Min. • 4 Personen",
+    description: "Mittel • 35 Min.",
     img: "/images/gnocchiauflauf.png",
     url: "gnocchiauflauf.html"
   },
   {
     title: "Portobello Caprese",
-    description: "Leicht • 20 Min. • 2 Personen",
+    description: "Leicht • 20 Min.",
     img: "/images/portobello.png",
     url: "portobello.html"
   },
   {
     title: "Bandnudeln mit Spinat und Lachs",
-    description: "Leicht • 20 Min. • 3 Personen",
+    description: "Leicht • 20 Min.",
     img: "/images/pastalachsspinat.png",
     url: "bandnudelnlachsspinat.html"
   },
   {
     title: "Spaghetti mit Kirschtomaten und Garnelen",
-    description: "Leicht • 30 Min. • 2 Personen",
+    description: "Leicht • 30 Min.",
     img: "/images/pastagarnelen.png",
     url: "spaghettigarnelen.html"
   },
   {
     title: "Wirsingrouladen",
-    description: "Mittel • 55 Min. • 3 Personen",
+    description: "Mittel • 55 Min.",
     img: "/images/wirsingroulade.png",
     url: "wirsingroulade.html"
   },
   {
     title: "Spitzkohlcurry",
-    description: "Leicht • 25 Min. • 4 Personen",
+    description: "Leicht • 25 Min.",
     img: "/images/spitzkohlcurry.png",
     url: "spitzkohlcurry.html"
   },
   {
     title: "Cannelloni mit Spargel",
-    description: "Leicht • 30 Min. • 2 Personen",
+    description: "Leicht • 30 Min.",
     img: "/images/cannellonispargel.png",
     url: "cannellonispargel.html"
   },
   {
     title: "Pasta mit Wirsing",
-    description: "Leicht • 30 Min. • 3 Personen",
+    description: "Leicht • 30 Min.",
     img: "/images/wirsingpasta.png",
     url: "wirsingpasta.html"
   },
   {
     title: "Albondigas",
-    description: "Leicht • 30 Min. • 4 Personen",
+    description: "Leicht • 30 Min.",
     img: "/images/albondigas.png",
     url: "albondigas.html"
   },
   {
     title: "Pilzrisotto",
-    description: "Mittel • 20 Min. • 2 Personen",
+    description: "Mittel • 20 Min.",
     img: "/images/pilzrisotto.png",
     url: "pilzrisotto.html"
   },
   {
     title: "Tomate Mozzarella Gnocchi",
-    description: "Leicht • 20 Min. • 3 Personen",
+    description: "Leicht • 20 Min.",
     img: "/images/gnocchimozzarella.png",
     url: "gnocchitomatemozarella.html"
   },
   {
     title: "Fenchel nepalesisch",
-    description: "Leicht • 20 Min. • 2 Personen",
+    description: "Leicht • 20 Min.",
     img: "/images/fenchelnepalesisch.png",
     url: "fenchelnepalesisch.html"
   },
   {
     title: "Spargel in rosa Basilikum-Sauce",
-    description: "Leicht • 30 Min. • 2 Personen",
+    description: "Leicht • 30 Min.",
     img: "/images/spargelkaesebasilikum.png",
     url: "spargelkaesesauce.html"
   },
   {
     title: "Auberginen-Curry mit Joghurtsauce",
-    description: "Leicht • 20 Min. • 2 Personen",
+    description: "Leicht • 20 Min.",
     img: "/images/aubergerinencurry.png",
     url: "auberginencurry.html"
   },
   {
     title: "Couscouspfanne",
-    description: "Leicht • 25 Min. • 2 Personen",
+    description: "Leicht • 25 Min.",
     img: "/images/couscouspfanne.png",
     url: "couscouspfanne.html"
   },
   {
     title: "Borsch  / Борщ",
-    description: "Mittel • 35 Min. • 4 Personen",
+    description: "Mittel • 35 Min.",
     img: "/images/borsch.png",
     url: "borsch.html"
   },
   {
     title: "Kohlrabi mit Sahnesauce",
-    description: "Leicht • 40 Min. • 4 Personen",
+    description: "Leicht • 40 Min.",
     img: "/images/kohlrabigemuese.png",
     url: "kohlgemuese.html"
   },
   {
     title: "Ratatouille",
-    description: "Mittel • 30 Min. • 4 Personen",
+    description: "Mittel • 30 Min.",
     img: "/images/ratatouille.png",
     url: "ratatouille.html"
   },
   {
     title: "Reibekuchen / Kartoffelpuffer",
-    description: "Leicht • 30 Min. • 2 Personen",
+    description: "Leicht • 30 Min.",
     img: "/images/kartoffelpuffer.png",
     url: "reibekuchen.html"
   },
   {
     title: "Frikadellen",
-    description: "Leicht • 25 Min. • 4 Personen",
+    description: "Leicht • 25 Min.",
     img: "/images/frikadelle.png",
     url: "frikadellen.html"
   },
   {
     title: "Spargelcremesuppe",
-    description: "Mittel • 30 Min. • 6 Personen",
+    description: "Mittel • 30 Min.",
     img: "/images/spargelcremesuppe.png",
     url: "spargelcremesuppe.html"
   },
   {
     title: "Feine Linsensuppe",
-    description: "Mittel • 50 Min. • 2 Personen",
+    description: "Mittel • 50 Min.",
     img: "/images/linsensuppe.png",
     url: "linsensuppe.html"
   },
   {
     title: "Enchiladas",
-    description: "Mittel • 1 Std. 15 Min. • 4 Personen",
+    description: "Mittel • 1 Std. 15 Min.",
     img: "/images/enchiladas.png",
     url: "enchiladas.html"
   },
   {
     title: "Spargelrisotto",
-    description: "Mittel • 55 Min. • 4 Personen",
+    description: "Mittel • 55 Min.",
     img: "/images/spargelrisotto.png",
     url: "spargelrisotto.html"
   },
   {
     title: "Buntes Ofengemüse",
-    description: "Leicht • 55 Min. • 4 Personen",
+    description: "Leicht • 55 Min.",
     img: "/images/ofengemuese.png",
     url: "buntesofengemuese.html"
   },
   {
     title: "Pasta Spargel",
-    description: "Leicht • 25 Min. • 2 Personen",
+    description: "Leicht • 25 Min.",
     img: "/images/pastaspargel.png",
     url: "spargelpasta.html"
   },
   {
     title: "Würziger Couscoussalat",
-    description: "Leicht • 35 Min. • 4 Personen",
+    description: "Leicht • 35 Min.",
     img: "/images/couscoussalat.png",
     url: "couscoussalat.html"
   },
   {
     title: "Inkasalat mit Avocado und Quinoa",
-    description: "Mittel • 35 Min. • 4 Personen",
+    description: "Mittel • 35 Min.",
     img: "/images/inkasalat.png",
     url: "inkasalat.html"
   },
   {
     title: "Mediterraner Kichererbsensalat",
-    description: "Mittel • 20 Min. • 2 Personen",
+    description: "Mittel • 20 Min.",
     img: "/images/kicherbsensalat.png",
     url: "kicherbsensalat.html"
   },
   {
     title: "Tortellinisalat",
-    description: "Mittel • 30 Min. • 4 Personen",
+    description: "Mittel • 30 Min.",
     img: "/images/tortelonisalat.png",
     url: "tortellinisalat.html"
   },
   {
     title: "Bulgursalat",
-    description: "Leicht • 1 Std. 30 Min. • 3 Personen",
+    description: "Leicht • 1 Std. 30 Min.",
     img: "/images/bulgursalat.png",
     url: "bulgursalat.html"
   },
   {
     title: "Guacamole",
-    description: "Leicht • 15 Min. • 4 Personen",
+    description: "Leicht • 15 Min.",
     img: "/images/guacamole.png",
     url: "guacamole.html"
   },
   {
     title: "Rote Bete mit Schafskäse",
-    description: "Leicht • 15 Min. • 4 Personen",
+    description: "Leicht • 15 Min.",
     img: "/images/rotebeteschafskaese.png",
     url: "rotebeteschafskaese.html"
   },
   {
     title: "Kartoffelsalat",
-    description: "Mittel • 40 Min. • 4 Personen",
+    description: "Mittel • 40 Min.",
     img: "/images/kartoffelsalat.png",
     url: "kartoffelsalat.html"
   },
   {
     title: "Tzatziki",
-    description: "Leicht • 10 Min. • 4 Personen",
+    description: "Leicht • 10 Min.",
     img: "/images/tzaziki.png",
     url: "tzatziki.html"
   },
