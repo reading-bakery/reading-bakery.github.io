9997192279c7071cce43fb1ac42d68e1ec1bdd8e
K.K. <katjakotschurin@gmail.com>
2026-02-22 17:04:58 +0100

    changed zufallsgenerator kochen

---
+++ zufallkochen.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const zufallBtn = document.getElementById("luckyZufall");
+
+    zufallBtn.addEventListener("click", () => {
+
+        const cards = document.querySelectorAll(".grid2 .card-rezept");
+
+        if (cards.length === 0) {
+            console.log("Keine Karten gefunden");
+            return;
+        }
+
+        // Zufällige Karte bestimmen
+        const randomIndex = Math.floor(Math.random() * cards.length);
+
+        cards.forEach((card, index) => {
+            if (index === randomIndex) {
+                card.style.display = "flex";   // deine Cards sind flex
+                card.scrollIntoView({
+                    behavior: "smooth",
+                    block: "center"
+                });
+            } else {
+                card.style.display = "none";
+            }
+        });
+
+    });
+
+});
